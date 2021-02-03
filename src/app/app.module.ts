4156df78da4622d7f01535f4f67e07d72e793dd5
JoelQ <joelquey@hotmail.com>
2021-02-03 17:06:44 +0800

    Added tailwindcss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
